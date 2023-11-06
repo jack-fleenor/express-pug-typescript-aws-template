96cb3517e50f6ee0022ade2945234b7cfd4ed265
Cody Fleenor <35227631+jack-fleenor@users.noreply.github.com>
2023-11-05 19:50:45 -0800

    Add Zod

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,7 @@
-export type QueryParams = {
-  id: string;
-};
+import { z } from 'zod';
+
+const QueryParamsSchema = z.object({
+  username: z.string(),
+});
+
+export type QueryParams = z.infer<typeof QueryParamsSchema>;
